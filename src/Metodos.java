5de3c5cadb56838a28daf463b0901dcbb388cd1b
Feyre <inaraoenning@gmail.com>
2025-09-18 20:17:53 -0300

    Add Funções

---
+++ src/Metodos.java
@@ -0,0 +1,58 @@
+import java.util.Scanner;
+
+public class Metodos {
+
+    public static int soma(int a, int b) {
+        return a + b;
+    }
+
+    public static int subtracao(int a, int b) {
+        return a - b;
+    }
+
+    public static int multiplicar(int a, int b) {
+        return a * b;
+    }
+
+    public static int dividir(int a, int b) {
+        return a / b;
+    }
+
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Informe a opção Desejada");
+        System.out.println("""
+                1- Soma
+                2- Subtração
+                3- Multiplicar
+                4- Dividir
+                :
+                """);
+        int opcao = sc.nextInt();
+
+
+        System.out.println("Primeiro valor: ");
+        int primeiroV = sc.nextInt();
+
+        System.out.println("Segundo Valor:");
+        int segundoV = sc.nextInt();
+
+
+        if (opcao == 1) {
+            System.out.println("Soma: " + primeiroV + "+" + segundoV + " = " + soma(primeiroV, segundoV));
+        } else if (opcao == 2) {
+            System.out.println("Subtrair: " + primeiroV + "-" + segundoV + " = " + subtracao(primeiroV, segundoV));
+        } else if (opcao == 3) {
+            System.out.println("Multiplicar: " + primeiroV + "*" + segundoV + " = " + multiplicar(primeiroV, segundoV));
+        } else {
+            System.out.println("Multiplicar: " + primeiroV + "/" + segundoV + " = " + dividir(primeiroV, segundoV));
+
+        }
+
+
+    }
+}
+
